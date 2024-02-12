71d5fd5057958fa4829e3a9f727bcfd97697a7c2
Vinayak Javalannavar <vinayak.javalannavar@matrixcos.com>
2024-02-13 00:57:59 +0530

    plugins added for file uploade select and bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxSelectModule } from 'ngx-select-ex';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FileUploadModule } from 'ng2-file-upload';
 
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    FileUploadModule,
+    FormsModule,
+    NgxSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
